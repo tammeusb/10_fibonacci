82744bab91902340115ba40f42d473b74340bf87
Brock <tammeusb@gmail.com>
2024-05-12 15:05:28 -0400

    Initial solution

--- fibonacci.js
+++ fibonacci.js
@@ -1,6 +1,31 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
 
+    let previousValue = 0;
+    let currentValue = 0;
+    
+    for (i = n; i > 0; i--) {
+        let notYetPreviousValue = currentValue;
+        currentValue = currentValue + previousValue;
+        if (currentValue == 0) currentValue = 1;   
+        previousValue = notYetPreviousValue;
+        console.log(currentValue);
+    }
+    return currentValue
 };
+
+//
+//will create the fibonacci sequence up to the arguent fibonacci(n)
+//and return the last number in the generated sequence.
+//e.g., fibonacci(3) ->
+//1,1,2
+//return 2
+
+//declare two variables outside of loop scope - previousValue and currentValue.
+//both values begin as 0. 
+//first loop, add 0 to 0 to return 0, if 0 make it 1.
+//next loo, add 0 to 1 to make 1.
+//need another variable to turn last currentValue into previousValue before
+//adding to changing currentValue.
 
 // Do not edit below this line
 module.exports = fibonacci;
